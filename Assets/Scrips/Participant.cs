7596ef7bf7eca599e1fcd65c86d31c392b771699
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2019-11-29 20:53:50 +0900

    繋げた

--- Assets/Scrips/Participant.cs
+++ Assets/Scrips/Participant.cs
@@ -6,7 +6,7 @@ public class Participant : MonoBehaviour {
     public void  NextScene(){
         //今いるシーンがTitleという名前であれば、Quizという名前のシーンに移動する
         if (Application.loadedLevelName == "Private") {
-            Application.LoadLevel ("participant");
+            Application.LoadLevel ("GuestEnter");
         }
     }
 }
