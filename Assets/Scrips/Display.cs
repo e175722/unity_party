8017cc015db0140db94a5420db9f9baac773877f
e175725 <e175725@ie.u-ryukyu.ac.jp>
2019-11-29 21:07:16 +0900

    試行錯誤中

---
+++ Assets/Scrips/Display.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display : MonoBehaviour
+{
+    
+//     GameObject test;
+//     UnityScript script;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         test = GameObject.Find("Test");
+//         script = test.GetComponent<UnityScript>();
+//     }
+// 
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         string userName = script.name;
+//         Debug.Log("ユーザー名は" + userName);
+//     }
+
+//         string temp;
+//         void Start(){
+//             temp = Test.GetName();
+//             print(temp);
+//         }
+}
---
+++ Assets/Scrips/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public static string name;
+    public InputField inputField;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = inputField.GetComponent<InputField>();
+        text = text.GetComponent<Text>();
+        text.text = inputField.text;
+        Debug.Log(text.text);
+    }
+    // Update is called once per frame
+//     public void InputText()
+//     {
+//         text.text = inputField.text;
+//         name = inputField.text;
+//     }
+//     
+//     public static string GetName(){
+//         return name;   
+//     }
+}
