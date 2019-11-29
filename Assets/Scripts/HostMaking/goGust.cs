890f9797f9d8bee644d417a5ab2cb19f748121f6
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2019-11-29 18:16:39 +0900

    サーバー接続, ホストゲストできた

---
+++ Assets/Scripts/HostMaking/goGust.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goGust : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void  NextScene(string resultMessage){
+        //今いるシーンがTitleという名前であれば、Quizという名前のシーンに移動する
+            Application.LoadLevel ("GuestEnter");
+    }
+}
