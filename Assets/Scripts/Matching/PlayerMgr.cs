0fe38250e3835781954c72402dcc4f8aa7c650a3
Takuma Motohara <e175721@ie.u-ryukyu.ac.jp>
2019-12-24 18:42:55 +0900

    ギットに負けた男たち

--- Assets/Scripts/Matching/PlayerMgr.cs
+++ Assets/Scripts/Matching/PlayerMgr.cs
@@ -5,24 +5,27 @@ using UnityEngine.UI;
 using Photon.Pun;
 using System;
 
-public class PlayerMgr : MonoBehaviour
+public class PlayerMgr : MonoBehaviour, Photon.Pun.IPunObservable
 {
 
     public Text PlayerName;
     public GameObject playerObj;
     public PhotonView photonView;
+    public Text Answer;
     
     // Start is called before the first frame update
     void Start()
     {    
         //プレイヤーネームのtext位置を取得
         Vector3 Ppos = PlayerName.transform.position;
+        Vector3 Ppos2 = Answer.transform.position;
         
         //photonViewオーナーの名前をテキスト表示
         PlayerName.text = photonView.Owner.NickName;
      
         //プレイヤーネームのtext位置を決定(入ってきた順に羅列されるように)
-        PlayerName.transform.position = new Vector3(Ppos.x, Ppos.y - (photonView.Owner.ActorNumber)*50, Ppos.z );         
+        PlayerName.transform.position = new Vector3(Ppos.x, Ppos.y - (photonView.Owner.ActorNumber)*50, Ppos.z );
+        Answer.transform.position = new Vector3(Ppos.x + 200, Ppos2.y - (photonView.Owner.ActorNumber)*50, Ppos2.z );     
     
         Debug.Log("プレイヤー名の座標" + PlayerName.transform.position);
         Debug.Log("点の座標 : " + Ppos);
@@ -31,5 +34,22 @@ public class PlayerMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    }    
+        if( photonView.IsMine == true ){
+            Answer.text = testInput.getAns();
+  }
+    }
+        
+    
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.IsWriting) {
+            // 自身側が生成したオブジェクトの場合は
+            // 色相値と移動中フラグのデータを送信する
+            stream.SendNext(Answer.text);
+        } else {
+            // 他プレイヤー側が生成したオブジェクトの場合は
+            // 受信したデータから色相値と移動中フラグを更新する
+            Answer.text = Convert.ToString(stream.ReceiveNext());
+
+        }
+    }
 }
