f4b984a5897ccdda98b389c6794c0c727fe240b1
KeisukeSDN <0xu79f313262y6k@ezweb.ne.jp>
2019-12-31 16:25:05 +0900

    問題出るようになった

---
+++ Assets/Scripts/Game/CreateQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateQuestion : MonoBehaviour
+{
+ public Text text;
+ private static string hiragana = "あいうえおかきくけこさしすせそたちつてとなにねのはひふへほまみむめもらりるれろやゆよわ";
+ private static string[] odai = new string[7] {"有名人", "夏の果物", "冬の果物", "夏の曲", "冬の曲", "今話題の人", "黒歴史"};
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        string sentense = "「" + hiragana[Random.Range(0, 42)] + "」で始まる" + odai[Random.Range(0, 6)] + "といえば";
+        
+        text.text = sentense;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
