46d9cd70fefae43f6448cf95806895ad5f787f9c
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2019-12-31 14:38:25 +0900

    とりあえずpush

--- Assets/Scripts/Matching/PlayerMgr.cs
+++ Assets/Scripts/Matching/PlayerMgr.cs
@@ -11,19 +11,19 @@ public class PlayerMgr : MonoBehaviour
     public Text PlayerName;
     public GameObject playerObj;
     public PhotonView photonView;
-    
+
     // Start is called before the first frame update
     void Start()
-    {    
+    {
         //プレイヤーネームのtext位置を取得
         Vector3 Ppos = PlayerName.transform.position;
-        
+
         //photonViewオーナーの名前をテキスト表示
         PlayerName.text = photonView.Owner.NickName;
-     
+
         //プレイヤーネームのtext位置を決定(入ってきた順に羅列されるように)
-        PlayerName.transform.position = new Vector3(Ppos.x, Ppos.y - (photonView.Owner.ActorNumber)*50, Ppos.z );         
-    
+        PlayerName.transform.position = new Vector3(Ppos.x, Ppos.y - (photonView.Owner.ActorNumber)*50, Ppos.z );
+
         Debug.Log("プレイヤー名の座標" + PlayerName.transform.position);
         Debug.Log("点の座標 : " + Ppos);
     }
@@ -31,5 +31,5 @@ public class PlayerMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    }    
+    }
 }
